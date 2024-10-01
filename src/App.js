063c191c98721317190c06899145575c29e5069d
amit113920 <amit.anand6263@gmail.com>
2024-10-01 23:26:44 +0530

    app.ls changes 2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://localhost:444/test/testDetail"
+          href="http://localhost:444/test/testDetail"
           target="_blank"
           rel="noopener noreferrer"
         >
