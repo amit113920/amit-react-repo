f02664e0e166d2273ef4e47a331906e818cf66ad
amit113920 <amit.anand6263@gmail.com>
2024-10-02 00:39:29 +0530

    app.ls changes 4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import {Layout} from '/layout/layout.js'
+import {Layout} from './layout/layout.js'
 function App() {
   return (
-    <Layout></Layout>
+    <Layout/>
   );
 }
 
