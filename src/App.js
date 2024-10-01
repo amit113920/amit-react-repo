9f772ad6eeb9692813659de28cca58a1f83d1a2d
amit113920 <amit.anand6263@gmail.com>
2024-10-02 00:32:41 +0530

    app.ls changes 3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
 import './App.css';
+import {Layout} from '/layout/layout.js'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-        <p>
-          Welcome to react project amit
-        </p>
-        <a
-          className="App-link"
-          href="http://localhost:444/test/getDetail"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout></Layout>
   );
 }
 
---
+++ src/services/security.js
@@ -0,0 +1,17 @@
+function Layout(){
+return(<>
+<div className="container">
+<p>header</p>
+</div>
+<div className="container">
+<p>container</p>
+</div>
+<div className="container">
+<p>footer</p>
+</div>
+
+</>
+);
+
+}
+export default Layout;
