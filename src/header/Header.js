480031a7f7688eada8ce3454bc904fc8ea24a39d
amit113920 <amit.anand6263@gmail.com>
2024-10-02 18:33:12 +0530

    commit 5

--- src/header/Header.js
+++ src/header/Header.js
@@ -11,14 +11,13 @@ return (
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
+      
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><Link onClick={()=>fetchData({url:'test/getDetail'})} >Detail</Link></li>
       <li class="active"><Link onClick={()=>fetchData({url:'test/getInfo'})} >Info</Link></li>
      
-      <li><a href="#">Page 2</a></li>
-      <li><a href="#">Page 3</a></li>
+      
     </ul>
   </div>
 </nav>
